9bc9861cc99639d68fdc68338d4b8665825ff746
brnkath <brnkath@gmail.com>
2023-07-19 16:36:46 -0500

    Update scripts prior to final push

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,36 +8,81 @@ budget_csv = os.path.join("PyBank", "Resources", "budget_data.csv")
 # Read in the CSV file
 with open(budget_csv, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-
+    # Skip header row
     header = next(csvreader)
 
-    # for row in csvreader:
+    # Set up variables
+    all_months = []
+    start_row = next(csvreader)
+    previous_row = int(start_row[1])
+    profit_losses = []
+    profit_loss_change = []
+    date_greatest_increase = ""
+    greatest_increase = ""
+    date_greatest_decrease = ""
+    greatest_decrease = ""
 
-# Print output to terminal
+    # Loop through rows in budget csv
+    for row in csvreader:
+        # Add each month to the month list
+        all_months.append(row[0])
+        # Add each date's profits and losses to profit_loss list
+        profit_losses.append(row[1])
+        profit_losses = [int(i) for i in profit_losses]
+        # Calculate the total of each profit/loss change
+        change_total = int(row[1]) - previous_row
+        previous_row = int(row[1])
+        profit_loss_change.append(change_total)
+
+    # Calculate the total months in the csv
+    total_months = len(all_months) + 1
+
+    # Calculate total of profits and losses
+    profit_losses = sum(profit_losses) + 1088983
+
+    # Caluclate the average change across all months in the csv
+    average_change = sum(profit_loss_change) / len(profit_loss_change)
+    average_change = round(average_change, 2)
+
+    # Calculate the greatest increase date and value
+    greatest_increase = max(profit_loss_change)
+    date_greatest_increase_index = profit_loss_change.index(greatest_increase)
+    date_greatest_increase = all_months[date_greatest_increase_index]
+
+    # Calculate the greatest decrease date and value
+    greatest_decrease = min(profit_loss_change)
+    date_greatest_decrease_index = profit_loss_change.index(greatest_decrease)
+    date_greatest_decrease = all_months[date_greatest_decrease_index]
+
+# Print output to the terminal
 print("Financial Analysis")
 print("------------------------------------------")
-print()
-print()
-print()
-print()
-print()
+print(f"Total Months: {total_months}")
+print(f"Total: ${profit_losses}")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {date_greatest_increase} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {date_greatest_decrease} (${greatest_decrease})")
 
 # Specify the file to write to
 output_path = os.path.join("PyBank", "Analysis", "budget_analysis.txt")
 
 # Open the file using write mode and set a variable to hold the contents
 with open(output_path, "w") as datafile:
-    # Write to the txt file
+    # Write to the text file
     datafile.write(f"Financial Analysis\n")
     datafile.write("\n")
     datafile.write("------------------------------------------\n")
     datafile.write("\n")
-    datafile.write(f"Total Months: \n")
+    datafile.write(f"Total Months: {total_months}\n")
     datafile.write("\n")
-    datafile.write(f"Total: \n")
+    datafile.write(f"Total: ${profit_losses}\n")
     datafile.write("\n")
-    datafile.write(f"Average Change: \n")
+    datafile.write(f"Average Change: ${average_change}\n")
     datafile.write("\n")
-    datafile.write(f"Greatest Increase in Profits: \n")
+    datafile.write(
+        f"Greatest Increase in Profits: {date_greatest_increase} (${greatest_increase})\n"
+    )
     datafile.write("\n")
-    datafile.write(f"Greatest Decrease in Profits: \n")
+    datafile.write(
+        f"Greatest Decrease in Profits: {date_greatest_decrease} (${greatest_decrease})\n"
+    )
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,26 +2,61 @@
 import os
 import csv
 
-# Path to the election_data csv in the Resources folder
+# Set up the path to the election_data csv in the Resources folder
 poll_csv = os.path.join("PyPoll", "Resources", "election_data.csv")
+
+
+# Set up a function to create a dictionary with each candidate's name and add a vote count
+def votes(candidate_info):
+    individual_candidate = candidate_info[2]
+
+    if individual_candidate in candidates:
+        candidates[individual_candidate] += 1
+    else:
+        candidates[individual_candidate] = 1
+    return candidates
+
+
+# Set up a function to calculate the percentage of votes each candidate received
+def perc_of_votes():
+    perc_of_votes = (candidates[individual_candidate] / total_votes) * 100
+    return perc_of_votes
+
 
 # Read in the CSV file
 with open(poll_csv, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-
+    # Skip header row
     header = next(csvreader)
 
-    # for row in csvreader:
+    # Set up variables
+    total_votes = 0
+    candidates = {}
+    high_percentage = 0
+    winner = ""
+    candidate_information = ""
 
-# Print output to terminal
-print("Election Results")
-print()
+    # Set up a loop to count the total number of votes and add individual candidate information to the candidates dictionary
+    for row in csvreader:
+        total_votes += 1
+        candidates = votes(row)
+
+    # Set up a loop to calculate each candidates percentage of the votes, determine the winner, and add candidate name, percentage, and vote count to the candidate information variable
+    for individual_candidate in candidates:
+        candidate_percentage = perc_of_votes()
+        if candidate_percentage > high_percentage:
+            high_percentage = candidate_percentage
+            winner = individual_candidate
+        candidate_information += f"{individual_candidate}: {candidate_percentage:.3f}% ({candidates[individual_candidate]})\n"
+
+# Print output to the terminal
+print(f"Election Results")
 print("------------------------------------------")
-print()
-print()
-print()
+print(f"Total Votes: {total_votes}")
 print("------------------------------------------")
-print()
+print(f"{candidate_information}")
+print("------------------------------------------")
+print(f"Winner: {winner}")
 print("------------------------------------------")
 
 # Specify the file to write to
@@ -29,21 +64,17 @@ output_path = os.path.join("PyPoll", "Analysis", "voting_analysis.txt")
 
 # Open the file using write mode and set a variable to hold the contents
 with open(output_path, "w") as datafile:
-    # Write to the txt file
+    # Write to the text file
     datafile.write(f"Election Results\n")
     datafile.write("\n")
     datafile.write("------------------------------------------\n")
     datafile.write("\n")
-    datafile.write(f"Total Votes: \n")
+    datafile.write(f"Total Votes: {total_votes}\n")
     datafile.write("\n")
     datafile.write("------------------------------------------\n")
     datafile.write("\n")
-    datafile.write(f"Charles Casper Stockham: \n")
+    datafile.write(f"{candidate_information}\n")
     datafile.write("\n")
-    datafile.write(f"Diana DeGette: \n")
-    datafile.write("\n")
-    datafile.write(f"Raymon Anthony Doane: \n")
-    datafile.write("\n")
-    datafile.write(f"Winner: \n")
+    datafile.write(f"Winner: {winner}\n")
     datafile.write("\n")
     datafile.write("------------------------------------------\n")
